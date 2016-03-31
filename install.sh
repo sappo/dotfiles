0af7b1283a0e30456be46aed894dd26b8fa46234
Kevin Sapper <mail@kevinsapper.de>
2016-03-31 12:27:08 +0200

    Calculate relative path between dotfiles and HOME correctly

--- install.sh
+++ install.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
+# Prints out the relative path between to absolute paths. Trivial.
+#
+# Parameters:
+# $1 = first path
+# $2 = second path
+#
+# Output: the relative path between 1st and 2nd paths
+relpath() {
+    local pos="${1%%/}" ref="${2%%/}" down=''
+
+    while :; do
+        test "$pos" = '/' && break
+        case "$ref" in $pos/*) break;; esac
+        down="../$down"
+        pos=${pos%/*}
+    done
+
+    echo "$down${ref##$pos/}"
+}
+
 function install_symlink
 {
     if [ ! -e $HOME/$2 ]; then
         cd $HOME
-        ln -s $1/$2
+        ln -s "$(relpath "$PWD" "$1")"/$2
         cd -
         echo "Installed symlink for $2";
     else
@@ -28,14 +48,14 @@ do
             install_symlink $PWD .gitconfig
             ;;
         "bashrc")
-            install_symlink $PWD .bash_profiles
+            install_symlink $PWD .bash_profile
             ;;
         "all")
             install_symlink $PWD .tmux.conf
             install_symlink $PWD .tmuxline
             install_symlink $PWD .vimrc
             install_symlink $PWD .gitconfig
-            install_symlink $PWD .bash_profiles
+            install_symlink $PWD .bash_profile
             break
             ;;
         "quit")
